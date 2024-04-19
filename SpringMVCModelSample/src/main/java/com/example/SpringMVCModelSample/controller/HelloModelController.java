6acd49be759b2b42031986467ce10bf52bb3709f
blackoceans <blackoceans@naver.com>
2024-04-19 11:48:16 +0900

    0419

---
+++ SpringMVCModelSample/src/main/java/com/example/SpringMVCModelSample/controller/HelloModelController.java
@@ -0,0 +1,23 @@
+package com.example.SpringMVCModelSample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("hello")
+
+public class HelloModelController {
+    //@GetMapping("model")
+    @GetMapping("bbbbb")
+    public String helloView(Model model) {
+        model.addAttribute("msg","타임리프");
+        model.addAttribute("name","길동");
+        //return "helloThymeleaf";
+        return "useThymeleaf";
+    }
+
+    //http://localhost:8080/hello/bbbbb 치면 나온다
+}
+
