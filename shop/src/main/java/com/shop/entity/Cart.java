c4c7b6334cfc669176a4228b8897483a4383e99b
blackoceans <blackoceans@naver.com>
2024-05-14 10:48:21 +0900

    0514

--- shop/src/main/java/com/shop/entity/Cart.java
+++ shop/src/main/java/com/shop/entity/Cart.java
@@ -11,13 +11,13 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class Cart {
+public class Cart extends BaseEntity{
     @Id
     @Column(name = "cart_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne //회원 엔티티랑 1:1연결
+    @OneToOne(fetch = FetchType.LAZY) //회원 엔티티랑 1:1연결
     @JoinColumn(name = "member_id")//연결할 외래키 지정
     private Member member;
 }
--- shop/src/main/java/com/shop/entity/Item.java
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -6,14 +6,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Table(name = "item")
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity{
     @Id
     @Column(name = "item_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -34,6 +32,5 @@ public class Item {
 
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; //상품 판매 상태
-    private LocalDateTime regTime; //등록 시간
-    private LocalDateTime updateTime; //수정 시간
+
 }
